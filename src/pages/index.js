38f251631a4f6891992cc2af82b62f1ea555b043
Mohammad-Quanit <muhammadquanit@gmail.com>
2021-01-05 23:42:54 +0500

    working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
-import { Link } from "gatsby";
 import * as React from "react";
-import Layout from "../components/layout";
 
 // markup
 const IndexPage = () => {
   return (
-    <Layout>
-      
-    </Layout>
+    <div>
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+        }}
+      >
+        <header>
+          <Navbar />
+        </header>
+        <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+          {children}
+        </div>
+      </div>
+      <footer style={footerStyle}>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://mohammad-quanit.github.io/" target="_blank" rel="noreferrer">
+          <span role="img" aria-label>💖️</span>
+        </a>
+      </footer>
+    </div>
   );
 };
 
