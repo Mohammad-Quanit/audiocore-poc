4953e46cc38a925ef5262ffb0593e54c2b586dcd
mohammad-quanit <muhammadquanit@gmail.com>
2021-01-26 23:05:59 +0500

    add: video.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const About = () => (
-  <Layout>
+const About = () => {
+  return (
+    <Layout>
     <SEO title="About" />
     <h1>Hi from About page</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-);
+  )
+};
 
 export default About;
---
+++ src/pages/video.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    // width: "700px",
+    // height: "315px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#___gatsby");
+
+const About = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {}
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <Layout>
+      <SEO title="About" />
+      <h1>Hi from About page</h1>
+      <div style={{ border: "2px solid black" }} onClick={openModal}>
+        Watch Video
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        shouldCloseOnOverlayClick={false}
+      >
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            onClick={closeModal}
+            style={{ width: "20px", height: "20px" }}
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        {/* <hr /> */}
+        <div>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/-SQt2pF52k0"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </Modal>
+    </Layout>
+  );
+};
+
+export default About;
